4d6c9d8a545a6ea0ef981b889600aef18778865d
Santiago Rocha <xxsantirocha12xx@gmail.com>
2023-08-28 21:44:41 -0500

    Ejemplo de JavaScript básico añadido.

---
+++ AREP-Taller2-RochaSantiago/webAppWithFiles/src/main/resources/jsEjemplo.js
@@ -0,0 +1,9 @@
+// Esto es un comentario de una línea en JavaScript
+
+// Definición de una función
+function showMessage() {
+    console.log("Hola, estás viendo este JavaScript de ejemplo");
+}
+
+// Llamada a la función para mostrar el mensaje
+showMessage();
